9ec1fdd94405e79a391e87b883e0ba5ddb1f2cbf
Greg Walker <gwalker@uship.com>
2014-09-17 17:12:45 -0500

    Move watcher to its own file

---
+++ LogstashTimer/BuildingFinishedWatcher.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace LogstashTimer
+{
+    public static class BuildingFinishedWatcher
+    {
+        static Mutex _mutex;
+
+        public static void CheckForWatcher()
+        {
+            bool result;
+            _mutex = new Mutex(true, "uShipTotalBuildTimeWatcher", out result);
+
+            if (!result)
+            {
+                //Do not run
+                Logger.Info("Already waiting, exiting...");
+                _mutex.Dispose();
+                return;
+            }
+
+            Process process = null;
+
+            try
+            {
+                var runningLocation = Process.GetCurrentProcess().MainModule.FileName;
+                if (!runningLocation.StartsWith(Path.GetTempPath()))
+                {
+                    var destFolder = TimeRecorder.BuildFilename("", "");
+                    try
+                    {
+                        foreach (var dllFile in Directory.EnumerateFiles(Path.GetDirectoryName(runningLocation))
+                                                 .Where(x => x.EndsWith("exe")
+                                                             || x.EndsWith("dll")
+                                                             || x.EndsWith("config")))
+                        {
+                            File.Copy(dllFile, Path.Combine(destFolder, Path.GetFileName(dllFile)), true);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //Failed copying... assume its either running or some other bad error
+                        return;
+                    }
+
+                    //run that exe
+                    process = new Process();
+                    process.StartInfo = new ProcessStartInfo
+                    {
+                        FileName = Path.Combine(destFolder, Path.GetFileName(runningLocation)),
+                        Arguments = "/totalWatcher",
+                        //RedirectStandardOutput = true,
+                        //UseShellExecute = false,
+                        //CreateNoWindow = true
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    };
+                }
+                else
+                {
+                    StartWatching();
+                }
+            }
+            finally
+            {
+                //Being explicit about releasing...
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();
+            }
+
+            if (process != null)
+            {
+                Logger.Info("Starting exe from temp");
+                process.Start();
+            }
+        }
+
+        private static void StartWatching()
+        {
+            //TODO For this to work, CheckForWatcher() needs to be the very first thing called
+            TotalBuildTimer.UpdateBuildStart();
+
+            //Error possiblities
+            var nullCount = 0;
+            var absoluteQuittingTime = DateTime.Now.AddMinutes(10);
+            var finishedTimeout = Settings.TotalBuildTimeTimeout;
+
+            while (true)
+            {
+                Logger.Info("Checking for non-updated last build time...");
+                var lastEnd = TotalBuildTimer.GetLastBuildEnd();
+                if (lastEnd.HasValue)
+                {
+                    if (lastEnd.Value < (DateTime.Now.Subtract(finishedTimeout)))
+                    {
+                        //Consider build end
+                        var counter = new BuildCounter();
+                        var buildNumber = counter.GetIncrementingBuildVersion(false, false);
+
+                        Logger.DisplayMessage("Submitting!:" + lastEnd.Value);
+
+                        TotalBuildTimer.SubmitBuildLength(buildNumber);
+                        return;
+                    }
+                }
+                else
+                {
+                    ++nullCount;
+                }
+                if (DateTime.Now > absoluteQuittingTime
+                    || nullCount >= 5)
+                {
+                    //Consider this some error
+                    return;
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(15));
+            }
+        }
+    }
+}
--- LogstashTimer/Program.cs
+++ LogstashTimer/Program.cs
@@ -27,122 +27,9 @@ namespace LogstashTimer
                         LogEvent(EventType.Finish, args);
                         break;
                     case "/totalWatcher":
-                        CheckForWatcher();
+                        BuildingFinishedWatcher.CheckForWatcher();
                         break;
                 }
-            }
-        }
-
-        static Mutex _mutex;
-
-        private static void CheckForWatcher()
-        {
-            bool result;
-            _mutex = new Mutex(true, "uShipTotalBuildTimeWatcher", out result);
-
-            if (!result)
-            {
-                //Do not run
-                Logger.Info("Already waiting, exiting...");
-                _mutex.Dispose();
-                return;
-            }
-
-            Process process = null;
-
-            try
-            {
-                var runningLocation = Process.GetCurrentProcess().MainModule.FileName;
-                if (!runningLocation.StartsWith(Path.GetTempPath()))
-                {
-                    var destFolder = TimeRecorder.BuildFilename("", "");
-                    try
-                    {
-                        foreach (var dllFile in Directory.EnumerateFiles(Path.GetDirectoryName(runningLocation))
-                                                 .Where(x => x.EndsWith("exe")
-                                                             || x.EndsWith("dll")
-                                                             || x.EndsWith("config")))
-                        {
-                            File.Copy(dllFile, Path.Combine(destFolder, Path.GetFileName(dllFile)), true);
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        //Failed copying... assume its either running or some other bad error
-                        return;
-                    }
-
-                    //run that exe
-                    process = new Process();
-                    process.StartInfo = new ProcessStartInfo
-                    {
-                        FileName = Path.Combine(destFolder, Path.GetFileName(runningLocation)),
-                        Arguments = "/totalWatcher",
-                        //RedirectStandardOutput = true,
-                        //UseShellExecute = false,
-                        //CreateNoWindow = true
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    };
-                }
-                else
-                {
-                    StartWatching();
-                }
-            }
-            finally
-            {
-                //Being explicit about releasing...
-                _mutex.ReleaseMutex();
-                _mutex.Dispose();
-            }
-
-            if (process != null)
-            {
-                Logger.Info("Starting exe from temp");
-                process.Start();
-            }
-        }
-
-        private static void StartWatching()
-        {
-            //TODO For this to work, CheckForWatcher() needs to be the very first thing called
-            TotalBuildTimer.UpdateBuildStart();
-
-            //Error possiblities
-            var nullCount = 0;
-            var absoluteQuittingTime = DateTime.Now.AddMinutes(10);
-            var finishedTimeout = Settings.TotalBuildTimeTimeout;
-
-            while (true)
-            {
-                Logger.Info("Checking for non-updated last build time...");
-                var lastEnd = TotalBuildTimer.GetLastBuildEnd();
-                if (lastEnd.HasValue)
-                {
-                    if (lastEnd.Value < (DateTime.Now.Subtract(finishedTimeout)))
-                    {
-                        //Consider build end
-                        var counter = new BuildCounter();
-                        var buildNumber = counter.GetIncrementingBuildVersion(false, false);
-
-                        Logger.DisplayMessage("Submitting!:" + lastEnd.Value);
-
-                        TotalBuildTimer.SubmitBuildLength(buildNumber);
-                        return;
-                    }
-                }
-                else
-                {
-                    ++nullCount;
-                }
-                if (DateTime.Now > absoluteQuittingTime
-                    || nullCount >= 5)
-                {
-                    //Consider this some error
-                    return;
-                }
-
-                Thread.Sleep(TimeSpan.FromSeconds(15));
             }
         }
 
@@ -156,7 +43,7 @@ namespace LogstashTimer
             switch (@event)
             {
                 case EventType.Start:
-                    CheckForWatcher();
+                    BuildingFinishedWatcher.CheckForWatcher();
                     //For build log to be accurate, Watcher needs to be running
                     //  Yay adding more possibilities for race conditions...
                     ////BuildLog.AddStartEntry(label);
